59ab980d3d93939283aa709e370832980cbf91ad
sunohuy <gabuavip123@gmail.com>
2025-03-08 21:31:45 +0700

    cộng 2 số nguyên

---
+++ cong2songuyen.py
@@ -0,0 +1,6 @@
+def add_two_digits (a,b):
+    return a+b
+a = int(input("nhập số thứ nhất :"))
+b = int(input("nhập số thứ hai : ")) 
+result = add_two_digits (a,b)
+print ("tổng 2 số cộng lại là : ", result)
