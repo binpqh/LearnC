ecab90481f8f3154207737554f65cbe99d804eb0
sunohuy <gabuavip123@gmail.com>
2025-03-11 20:44:54 +0700

    class.py
    a

--- cong2songuyen.py
+++
@@ -1,6 +0,0 @@
-def add_two_digits (a,b):
-    return a+b
-a = int(input("nhập số thứ nhất :"))
-b = int(input("nhập số thứ hai : ")) 
-result = add_two_digits (a,b)
-print ("tổng 2 số cộng lại là : ", result)
